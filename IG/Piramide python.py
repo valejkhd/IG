eaf0a69614a9ff005d4df0136084c05ad7f71cab
valejkhd <valentinavanegas076@gmail.com>
2026-07-08 13:22:02 -0500

    Delete IG directory

--- IG/Piramide python.py
+++
@@ -1,5 +0,0 @@
-a= float(input("Ingrese a: "))
-b= float(input("Ingrese b: "))
-h= float(input("Ingrese c: "))
-volumen= (a*b*h)/3
-print("El volumen de la piramide es: ", volumen)
--- IG/Ventadeleche.py
+++
@@ -1,6 +0,0 @@
-litros= float(input("Ingrese la cantidad de litros vendidos:  "))
-precio= float(input("Ingrese el precio por litro:  "))
-galones= litros/3.78541
-precio= galones*precio
-print("El precio total de la venta de leche es: ", precio)
-print("La cantidad de galones vendidos es: ", galones)
